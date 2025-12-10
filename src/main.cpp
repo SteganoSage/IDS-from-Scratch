62e73fe18ef8bec98975cc6586e06224e9112bec
Dhruv <dhruvmanand0729@gmail.com>
2025-12-10 19:24:20 +0000

    Initiated a pcap capture device

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include<pcap.h>
 
-int main(){
-	std::cout << "[IDS] Starting IDS from scratch..." << std::endl;
+
+
+int main(int argc, char* argv[]){
+
+	char errbuff[PCAP_ERRBUF_SIZE];
+	const char* device = nullptr;
+
+	if(argc > 1) device = argv[1];
+	else device = pcap_lookupdev(errbuff);
+
+	if(!device){
+		std::cerr << "pcap_lookupdev failed :" << errbuff << std :: endl;
+		return 1;
+	}
+
+	std::cout << "[+] Using Device : "<< device << std::endl;
+
+	pcap_t* handle = pcap_open_live(device,65535,1,1000,errbuff);
+
+	if(!handle){
+		std::cerr << "pcap_open_live failed"<< errbuff << std::endl;
+		return 1;
+	}
+
+	std::cout << "[+] Opened handle \n";
+	pcap_close(handle);
 	return 0;
+
 }
